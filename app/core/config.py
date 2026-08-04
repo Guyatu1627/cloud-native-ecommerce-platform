e3fc931cfe855e8895571d9a16e93af84bd6b113
Guyatu1627 <Guyatugd@gmail.com>
2026-08-04 16:15:48 +0300

    chore: configure database session and initialize alembic

--- app/core/config.py
+++ app/core/config.py
@@ -4,6 +4,7 @@ class Settings(BaseSettings):
     PROJECT_NAME: str = "Cloud-Native E-Commerce API"
     VERSION: str = "1.0.0"
     API_V1_STR: str = "/api/v1"
+    DATABASE_URL: str = "postgresql://postgres:postgres@localhost:5432/ecommerce_db"
 
     class Config:
         case_sensitive = True
